fcd67151f9c4e5234e7d9cba1b42a18a43f4965e
Mike Babb <mike7400@gmail.com>
2020-07-30 01:54:45 -0400

    Initial development commit with quite a few changes 🤦‍♂️

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 `dollar.ts`💲 takes this:
 
-```js
+```ts
 const el: HTMLElement = document.querySelector(".class");
 el.innerHTML = "hi mom!";
 el.style.transform = "translateX(100px)";
@@ -14,7 +14,7 @@ el.setAttribute("msg", "hi dad!");
 
 and transforms it to this:
 
-```js
+```ts
 $(".class").setattr({
     html: "hi mom!",
     styles: {
@@ -25,7 +25,7 @@ $(".class").setattr({
 });
 ```
 
-**WHAT** more lines?! Yep. Oh, and notice the omission of a type hint for the latter. kool 🧊
+**WHAT** more lines?! Yep. Oh, and notice the omission of a type hint for the latter. kool 🧊.
 
 ## Introit
 
@@ -35,7 +35,7 @@ The rather verbose syntax of
 
 caused a recent project of mine to become cluttered and downright unintelligible in places. Additionally, the common operation of modifying CSS attributes was a major pain:
 
-```js
+```ts
 el.style.$prop = `${value}`;
 // or dynamically, as
 el.style.setProperty(prop, value[prop]);
@@ -43,7 +43,7 @@ el.style.setProperty(prop, value[prop]);
 
 Inspired, I spent a few hours creating yet another jQuery replacement, `YAJR`, with its one and only library file [dollar.ts](src/dollar.ts).The goal of this was to turn something like this:
 
-```js
+```ts
 Array.from(document.querySelectorAll(".class")).forEach((el) => {
     el.style.height = "100px";
 });
@@ -51,23 +51,19 @@ Array.from(document.querySelectorAll(".class")).forEach((el) => {
 
 into this:
 
-```js
+```ts
 $$(".class").css({ height: "100px" });
 ```
 
 _The double dollar `$$` syntax will be explained later._
 
-While also providing the type safety and code completion the former offers through standard TypeScript.
-
-The end product does just that, but comes with a few caveats:
+While also providing the type safety and code completion the former offers through standard TypeScript. The end product does just that, but comes with a few caveats:
 
 ##### Return Type Specialization
 
-The element type returned by `$(".class")` is of type `IDollarElement & Element`. In short, if you want to specialize the type returned by a `$` call, you must `&` it with the type `IDollarElement`.
+The element type returned by `$(".class")` is of type `IDollarElement & Element`. In short, if you want to specialize the type returned by a `$` call, you must `&` it with the type `IDollarElement`. An example ensuring type safety when dealing with an `HTMLCanvas` element:
 
-An example ensuring type safety when dealing with an `HTMLCanvas` element:
-
-```js
+```ts
 const canvasEl: HTMLCanvasElement & IDollarElement = $("canvas");
 ...
 ```
@@ -82,8 +78,59 @@ To note, this isn't entirely necessary. But if you, like me, want type hinting a
 
     -   `on`: suped-up version of `addEventListener` that handles multiple events (separated by a space).
     -   `off`: `removeEventListener` variant of the above.
-    -   `setattr`: shorthand for setting multiple attributes with an `Attribute` typed object.
+    -   `setattr`: shorthand for setting multiple attributes with an `Attribute` typed object. Smarter in the sense that it can detect specialized `Element` types and set their attributes accordingly.
     -   `css`: shorthand for `$.setattr({style: {...}})`
     -   `html`: shorthand for `$.setattr({html: ...})`
 
--   😄 An easy way to confound and confuse people into thinking you're using jQuery.
+-   😄 An easy way to confound and confuse people into thinking you're using jQuery. But of course you're not, it's `current_year`! (satire 🙄)
+
+## Examples
+
+### Set the color of an element with the class of `money`:
+
+```ts
+$(".money").css({ color: "red" });
+```
+
+### Set the height of all elements with the class of `dollar` to `50%` and add event listeners:
+
+```ts
+$$(".dollar").css({ height: "50%" }).on("click mousedown", (ev) => {...});
+```
+
+### Add an event listener to the window object for mobile and desktop mouse events:
+
+_why this is normally so complicated is beyond me_
+
+```ts
+$(window).on("click touchstart", (ev) => {...})
+```
+
+### Using the `setattr` specialization functionality to set the height of an `HTMLCanvasElement`.
+
+First we create a specialized version of `$`:
+
+```ts
+const canvasSpec = {
+    height: (context, value) => {
+        // type checking here is not 100% necessary, but this is TypeScript...
+        if (context instanceof HTMLCanvasElement && typeof value === "string") {
+            context.height = parseFloat(value);
+        }
+    }
+};
+
+const $canvas = (query, context?) => {
+    return $<HTMLCanvasElement>(query, context, canvasSpec);
+};
+```
+
+Notice the type hint we gave in the return statement. The output object will now inherit all types from `HTMLCanvasElement`. neat.
+
+We can then call it like so:
+
+```ts
+$canvas("canvas").setattr({ height: "100" });
+```
+
+And the correct attribute will be set!
--- src/dollar.ts
+++ src/dollar.ts
@@ -1,92 +1,154 @@
-type DollarElement = IDollarElement & Element;
+type NestedAttribute = { [s: string]: string } | string;
+interface Attributes {
+    [s: string]: NestedAttribute;
+    style?: { [s: string]: string };
+    html?: string;
+}
+
+interface AttributeSpec<T> {
+    contextType: (context: any) => boolean;
+    attrs: Map<RegExp, (key: string, value: NestedAttribute, context: T) => void>;
+}
 
 interface IDollarElement extends Element {
-    on: (name: string, func: (event: Event) => void) => DollarElement;
-    off: (name: string, func: (event: Event) => void) => DollarElement;
-    setattr: (attrs: object) => DollarElement;
-    css: (attrs: object) => DollarElement;
-    html: (attrs: object) => DollarElement;
+    on: (name: string, func: (event: Event) => void) => IDollarElement;
+    off: (name: string, func: (event: Event) => void) => IDollarElement;
+    setattr: (attrs: Attributes) => IDollarElement;
+    css: (attrs: NestedAttribute) => IDollarElement;
+    html: (attrs: NestedAttribute) => IDollarElement;
 }
 
 const on = function (
     names: string,
     func: (event: Event) => void,
     context: EventTarget
-): EventTarget {
+) {
     names.split(" ").forEach((event) => {
         context.addEventListener(event, func);
     });
-    return this;
+    return context;
 };
 
 const off = function (
     names: string,
     func: (event: Event) => void,
     context: EventTarget
-): EventTarget {
+) {
     names.split(" ").forEach((event) => {
         context.removeEventListener(event, func);
     });
-    return this;
+    return context;
 };
 
-const htmlElementSpecializations = function (
-    key: string,
-    value: any,
-    context: Element
-) {
-    if (context instanceof HTMLElement) {
-        if ((key === "styles" || key === "style") && typeof value === "object") {
-            for (const prop in value) {
-                context.style.setProperty(prop, value[prop]);
+const htmlElementSpec: AttributeSpec<HTMLElement> = {
+    contextType: (context) => context instanceof HTMLElement,
+    attrs: new Map([
+        [
+            RegExp("html"),
+            (key, value, context) => {
+                if (typeof value === "string") {
+                    context.innerHTML = value;
+                }
             }
-        } else if (key === "html") {
-            context.innerHTML = value;
-        }
-        return true;
-    } else {
-        return false;
-    }
+        ],
+        [
+            RegExp("styles?"),
+            (key, value, context) => {
+                if (typeof value === "object") {
+                    Object.entries(value).forEach(([subkey, subvalue]) => {
+                        context.style.setProperty(subkey, subvalue);
+                    });
+                }
+            }
+        ]
+    ])
 };
 
-const setAttributes = function (attrs: object, context: Element) {
-    for (const [key, value] of Object.entries(attrs)) {
-        if (value === null) {
-            context.removeAttribute(key);
-        } else {
-            context.setAttribute(key, value);
-        }
-        htmlElementSpecializations(key, value, context);
-    }
-    return this;
+const elementSpec: AttributeSpec<Element> = {
+    contextType: (context) => context instanceof Element,
+    attrs: new Map([
+        [
+            RegExp(".*"),
+            (key, value, context) => {
+                if (value == null) {
+                    context.removeAttribute(key);
+                } else if (typeof value === "string") {
+                    context.setAttribute(key, value);
+                }
+            }
+        ]
+    ])
 };
 
-const dollarFunctions = {
-    on: function (name: string, func: (event: Event) => void) {
-        return on(name, func, this);
-    },
-    off: function (name: string, func: (event: Event) => void) {
-        return off(name, func, this);
-    },
-    setattr: function (attrs: Object) {
-        return setAttributes(attrs, this);
-    },
-    css: function (attrs: Object) {
-        return setAttributes(
-            {
-                styles: attrs
-            },
-            this
-        );
-    },
-    html: function (s: string) {
-        return setAttributes(
-            {
-                html: s
-            },
-            this
-        );
-    }
+/**
+ * Sets the attributes of a context element based on the `attrs` Attribute object.
+ *
+ * Important to note is the func callback, which is used from attribute key specialization.
+ * HTML Elements have a hard-coded set of reserved keys, such `html`: if the input key is `html`
+ * and the input context is of type HTMLElement, the keyword is specialized, and the context
+ * element's `innerHTML` attribute is modified.
+ *
+ * @param attrs input attributes.
+ * @param context context Element.
+ * @param func function to execute at each [key, value] call.
+ */
+const setAttributes = function <T>(
+    attrs: Attributes,
+    context,
+    specs?: Array<AttributeSpec<T>>
+) {
+    Object.entries(attrs).forEach(([key, value]) => {
+        specs.forEach((spec) => {
+            const isOfType = spec.contextType(context);
+
+            if (isOfType) {
+                spec.attrs.forEach((specFunc, specKey) => {
+                    if (specKey.test(key)) {
+                        specFunc(key, value, context);
+                    }
+                });
+            }
+        });
+    });
+
+    return context;
+};
+
+const createDollarFunctions = function <T>(spec?: AttributeSpec<T | Element>) {
+    const setattr = function (attrs: Attributes, context) {
+        return setAttributes(attrs, context, [spec, htmlElementSpec, elementSpec]);
+    };
+
+    return {
+        on: function (name: string, func: (event: Event) => void) {
+            return on(name, func, this);
+        },
+        off: function (name: string, func: (event: Event) => void) {
+            return off(name, func, this);
+        },
+
+        setattr: function (attrs: Attributes) {
+            return setattr(attrs, this);
+        },
+
+        css: function (styles: NestedAttribute) {
+            return setattr(
+                {
+                    styles
+                },
+                this
+            );
+        },
+
+        html: function (s: string) {
+            return setattr(
+                {
+                    html: s
+                },
+                this
+            );
+        }
+    };
 };
 
 const foldFunctions = function (funcs) {
@@ -103,40 +165,69 @@ const foldFunctions = function (funcs) {
     return folded;
 };
 
-const dollarFoldedFunctions = foldFunctions(dollarFunctions);
+/**
+ * Like `$`, `$$` is essentially a wrapper for .querySelectorAll, which notoriously returns
+ * a NodeList object.
+ *
+ * In the case of an Array or NodeListOf<Element>, we simply pipe the nodes to
+ * Array.from and preserve there attributes. In the case of a string, we call
+ * `context.querySelectorAll(query)` and do the same.
+ *
+ * After completion, we add the set of IDollarElement prototype functions to the resultant object,
+ * but with the special functionality of looping over each element, and then executing
+ * the function call (hence dollarFoldedFunctions; they're folded over the array).
+ *
+ * @param query query element[s] or a query string.
+ * @param context context element or document.
+ */
+// function $$<T>(
+//     query: NodeListOf<Element> | Array<T | Element> | string,
+//     context: Document | Element = document,
+//     spec?: AttributeSpec
+// ) {
+//     const nodes =
+//         query instanceof NodeList || query instanceof Array
+//             ? query
+//             : typeof query === "string"
+//                 ? context.querySelectorAll(query)
+//                 : [query];
 
-function $$(
-    query: NodeListOf<Element> | Array<Element> | string,
-    context: Document | Element = document
-) {
-    const nodes =
-        query instanceof NodeList || query instanceof Array
-            ? query
-            : typeof query === "string"
-            ? context.querySelectorAll(query)
-            : [query];
+//     if (nodes == null) {
+//         return undefined;
+//     } else {
+//         const arr = Array.from(nodes).map((el) =>
+//             Object.assign(el, createDollarFunctions(spec))
+//         );
+//         return Object.assign(arr, foldFunctions(createDollarFunctions(spec)));
+//     }
+// }
 
-    if (nodes == null) {
-        return undefined;
-    } else {
-        const arr = Array.from(nodes).map((el) => Object.assign(el, dollarFunctions));
-        return Object.assign(arr, dollarFoldedFunctions);
-    }
-}
+// Function decelerations to aid in proper type hinting.
+// function $<T = string>(
+//     query: string,
+//     context?: Document | Element,
+//     spec?: AttributeSpec
+// ): Element;
+// function $<T>(query: T, context?: Document | Element, spec?: AttributeSpec);
 
-function $(query: string, context?: Document | Element): IDollarElement & Element;
-function $<T>(query: T, context?: Document | Element): IDollarElement & T;
-
-function $<T>(query: T, context = document) {
+/**
+ * The dollar 💲in dollar.ts. Wrapper around .querySelector in the case of an input
+ * query string, and a simple pipe in the case of anything else (usually of type Element).
+ *
+ * The resultant object is an intersection of T & IDollarElement. So if the input object is
+ * of type HTMLElement, that type is preserved and &'d with IDollarElement.
+ *
+ * @param query input query string or element.
+ * @param context context element or document.
+ */
+function $<T>(query: T, context = document, spec?: AttributeSpec): T & IDollarElement {
     const node = typeof query === "string" ? context.querySelector(query) : query;
 
     if (node == null) {
         return undefined;
     } else {
-        return Object.assign(node, dollarFunctions);
+        return Object.assign(node, createDollarFunctions(spec));
     }
 }
 
-export { $, $$ };
-
-export { IDollarElement };
+export { $ };
